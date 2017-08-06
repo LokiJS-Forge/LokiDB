f785d227df658005a61041a98ec46620b8be0847
Viatorus <lutztonineubert@gmail.com>
2017-08-06 10:20:18 +0200

    build: improve build correctness (#4)
    * Remove tmp coverage file from project.
    * Fix eslint.
    * Improve build.js

--- packages/test/spec/generic/test.spec.js
+++ packages/test/spec/generic/test.spec.js
@@ -1,12 +1,12 @@
 /* global describe, it, expect */
-import {func} from '../../src/index';
+import {func} from "../../src/index";
 
-describe('autoupdate', () => {
-  it('auto updates inserted documents', () => {
+describe("autoupdate", () => {
+  it("auto updates inserted documents", () => {
     expect(func()).toEqual(400);
   });
 
-  it('auto updates inserted documents2', () => {
+  it("auto updates inserted documents2", () => {
     expect(func()).toEqual(400);
   });
 });
