8c8088e82471642e3c6d2d790595fd99fea4a4ba
Viatorus <lutztonineubert@gmail.com>
2017-09-27 22:01:01 +0200

    feat(loki): add optional flag for Changes API to limit update operation output to modified properties only (#29)
    Also includes properties flagged as unique, since they would be needed to identify the item that is updated. (from LokiJS #578)

--- packages/loki/src/event_emitter.js
+++ packages/loki/src/event_emitter.js
@@ -13,24 +13,24 @@ export class LokiEventEmitter {
 
   constructor() {
     /**
-		 * @prop {hashmap} events - a hashmap, with each property being an array of callbacks
-		 */
+     * @prop {hashmap} events - a hashmap, with each property being an array of callbacks
+     */
     this.events = {};
 
     /**
-		 * @prop {boolean} asyncListeners - boolean determines whether or not the callbacks associated with each event
-		 * should happen in an async fashion or not
-		 * Default is false, which means events are synchronous
-		 */
+     * @prop {boolean} asyncListeners - boolean determines whether or not the callbacks associated with each event
+     * should happen in an async fashion or not
+     * Default is false, which means events are synchronous
+     */
     this.asyncListeners = false;
   }
 
   /**
-	 * on(eventName, listener) - adds a listener to the queue of callbacks associated to an event
-	 * @param {string|string[]} eventName - the name(s) of the event(s) to listen to
-	 * @param {function} listener - callback function of listener to attach
-	 * @returns {int} the index of the callback in the array of listeners for a particular event
-	 */
+   * on(eventName, listener) - adds a listener to the queue of callbacks associated to an event
+   * @param {string|string[]} eventName - the name(s) of the event(s) to listen to
+   * @param {function} listener - callback function of listener to attach
+   * @returns {int} the index of the callback in the array of listeners for a particular event
+   */
   on(eventName, listener) {
     let event;
 
@@ -50,21 +50,21 @@ export class LokiEventEmitter {
   }
 
   /**
-	 * emit(eventName, data) - emits a particular event
-	 * with the option of passing optional parameters which are going to be processed by the callback
-	 * provided signatures match (i.e. if passing emit(event, arg0, arg1) the listener should take two parameters)
-	 * @param {string} eventName - the name of the event
-	 * @param {object} data - optional object passed with the event
-	 */
-  emit(eventName, data) {
+   * emit(eventName, data) - emits a particular event
+   * with the option of passing optional parameters which are going to be processed by the callback
+   * provided signatures match (i.e. if passing emit(event, arg0, arg1) the listener should take two parameters)
+   * @param {string} eventName - the name of the event
+   * @param {object} data - optional object passed with the event
+   */
+  emit(eventName, ...data) {
     if (eventName && this.events[eventName]) {
       this.events[eventName].forEach((listener) => {
         if (this.asyncListeners) {
           setTimeout(() => {
-            listener(data);
+            listener(...data);
           }, 1);
         } else {
-          listener(data);
+          listener(...data);
         }
 
       });
@@ -72,21 +72,21 @@ export class LokiEventEmitter {
   }
 
   /**
-	 * Alias of LokiEventEmitter.prototype.on
-	 * addListener(eventName, listener) - adds a listener to the queue of callbacks associated to an event
-	 * @param {string|string[]} eventName - the name(s) of the event(s) to listen to
-	 * @param {function} listener - callback function of listener to attach
-	 * @returns {int} the index of the callback in the array of listeners for a particular event
-	 */
+   * Alias of LokiEventEmitter.prototype.on
+   * addListener(eventName, listener) - adds a listener to the queue of callbacks associated to an event
+   * @param {string|string[]} eventName - the name(s) of the event(s) to listen to
+   * @param {function} listener - callback function of listener to attach
+   * @returns {int} the index of the callback in the array of listeners for a particular event
+   */
   addListener(eventName, listener) {
     return this.on(eventName, listener);
   }
 
   /**
-	 * removeListener() - removes the listener at position 'index' from the event 'eventName'
-	 * @param {string|string[]} eventName - the name(s) of the event(s) which the listener is attached to
-	 * @param {function} listener - the listener callback function to remove from emitter
-	 */
+   * removeListener() - removes the listener at position 'index' from the event 'eventName'
+   * @param {string|string[]} eventName - the name(s) of the event(s) which the listener is attached to
+   * @param {function} listener - the listener callback function to remove from emitter
+   */
   removeListener(eventName, listener) {
     if (Array.isArray(eventName)) {
       eventName.forEach((currentEventName) => {
