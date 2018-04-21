dfd6ed350c4c0bff8f59b4eb310a1b7d47c391c6
Viatorus <lutztonineubert@gmail.com>
2018-04-21 22:25:58 +0200

    Wrap older database version intoa a proxy.

--- packages/loki/spec/generic/legacy_load.spec.ts
+++ packages/loki/spec/generic/legacy_load.spec.ts
@@ -2,15 +2,83 @@
 
 // declare var loki: any;
 
-describe("testing legacy loader", function () {
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 
-  it("test", () => {
+interface DatabaseVersion {
+  version: number;
+}
+
+interface Serialized {
+  version: 2;
+  name: string;
+  value: number;
+  sub: SubSerialized;
+}
+
+interface SubSerialized {
+  work: number[];
+  query: boolean;
+  nested: {
+    get: number;
+    float: number;
+    nester: {
+      oh: boolean;
+      wow?: null;
+    }
+  };
+}
+
+interface Serialized2 {
+  version: 1;
+  name: string;
+  sub: SubSerialized2;
+}
+
+interface SubSerialized2 {
+  work: number[];
+}
+
+function get<T, U extends object = object>(t: T, other?: U): (T & U) extends Omit<Serialized, "version"> ? Serialized : never {
+  let l = new Proxy({},
+    {
+      get: (obj, prop) => {
+        if (obj.hasOwnProperty(prop)) {
+          return obj[prop];
+        } else if (other && other.hasOwnProperty(prop)) {
+          if (other[prop] !== null && typeof other[prop] === "object") {
+            return get(t ? t[prop]: undefined, other[prop]);
+          } else {
+            return other[prop];
+          }
+        } else if (t && t.hasOwnProperty(prop)) {
+          return t[prop];
+        }
+      }
+    }
+  );
+  return l as any;
+}
+
+fdescribe("testing legacy loader", function () {
+
+  fit("test", () => {
+
+    let serialized2: Serialized2 = {
+      version: 1,
+      name: "abc",
+      sub: {work: [1]}
+    };
+    let serialized1 = get(serialized2, {version: 2, value: 1, sub: {query: true, nested: {get: 2, float: 3.0, nester: {oh: true}}}});
+    console.log(serialized1.version);
+
+
+
     // const legacyDB = new loki();
     // {
     //   const abc = legacyDB.addCollection("abc", {indices: ["a", "c"], unique: ["b"]});
     //   abc.insert([
     //     {
-    //       a: 1, b: 2, c: 1, d: {
+    //       a: 1, b: 2,/*/ c: 1, d: {
     //         msg: "hello"
     //       }
     //     },
