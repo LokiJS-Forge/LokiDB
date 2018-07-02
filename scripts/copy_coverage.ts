1346de01953def0eff6dd0f530f745df84bd05d2
Viatorus <lutztonineubert@gmail.com>
2018-07-02 13:03:45 +0200

    Use istanbul for merge and nyc for export.

--- scripts/copy_coverage.ts
+++
@@ -1,6 +0,0 @@
-import {copy, makeDir} from "./common";
-
-makeDir("coverage/tmp");
-makeDir("coverage/combined");
-copy("coverage/karma/coverage-final.json", "coverage/tmp/karma.coverage.json");
-copy("coverage/node/coverage-final.json", "coverage/tmp/node.coverage.json");
